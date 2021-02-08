26a07e53f2d4db077de3c5c056201e9bf4bac5d3
silvercrow185 <oossllyy14@gmail.com>
2021-02-08 17:48:42 -0500

    botones del home

--- src/plantillas/home.php
+++ src/plantillas/home.php
@@ -47,9 +47,14 @@
         <div class="page-wrapper">
             <div class="conteiner-fluid">
             <!-- AQUI EMPEZAMOS A AGREGAR DISEÑO DEL CENTRO -->
-
-
-
+            <button type="button" class="btn btn-primary" style='width:280px; height:230px; margin:10px'><i class="fas fa-tag"></i><p></p>Ventas</button>
+   			<button type="button" class="btn btn-secondary" style='width:200px; height:150px; margin:10px'><i class="fas fa-gift"></i><p></p>Promociones</button>
+			<button type="button" class="btn btn-success" style='width:200px; height:150px; margin:10px'><i class="fas fa-pallet"></i><p></p>Inventario</button>
+			<button type="button" class="btn btn-info" style='width:200px; height:150px; margin:10px'><i class="fas fa-clipboard-list"></i><p></p>Reportes</button>
+			<button type="button" class="btn btn-warning" style='width:200px; height:150px; margin:10px'><i class="fas fa-users"></i><p></p>Clientes</button>
+			<button type="button" class="btn btn-danger" style='width:200px; height:150px; margin:10px'><i class="fas fa-donate"></i><p></p>Compras</button>
+			<button type="button" class="btn btn-light" style='width:200px; height:150px; margin:10px'><i class="fas fa-truck-moving"></i><p></p>Proveedores</button>
+			<button type="button" class="btn btn-dark" style='width:200px; height:150px; margin:10px'><i class="fas fa-chart-line"></i><p></p>Finanzas</button>
             </div>
         </div>
 
