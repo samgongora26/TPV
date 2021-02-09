cf7d8ca673aedd1e85434aec970971ee17895cf8
silvercrow185 <oossllyy14@gmail.com>
2021-02-08 20:28:23 -0500

    links en los botones

--- src/plantillas/home.php
+++ src/plantillas/home.php
@@ -47,14 +47,14 @@
         <div class="page-wrapper">
             <div class="conteiner-fluid">
             <!-- AQUI EMPEZAMOS A AGREGAR DISEÑO DEL CENTRO -->
-            <button type="button" class="btn btn-primary" style='width:280px; height:230px; margin:10px'><i class="fas fa-tag"></i><p></p>Ventas</button>
-   			<button type="button" class="btn btn-secondary" style='width:200px; height:150px; margin:10px'><i class="fas fa-gift"></i><p></p>Promociones</button>
-			<button type="button" class="btn btn-success" style='width:200px; height:150px; margin:10px'><i class="fas fa-pallet"></i><p></p>Inventario</button>
-			<button type="button" class="btn btn-info" style='width:200px; height:150px; margin:10px'><i class="fas fa-clipboard-list"></i><p></p>Reportes</button>
-			<button type="button" class="btn btn-warning" style='width:200px; height:150px; margin:10px'><i class="fas fa-users"></i><p></p>Clientes</button>
-			<button type="button" class="btn btn-danger" style='width:200px; height:150px; margin:10px'><i class="fas fa-donate"></i><p></p>Compras</button>
-			<button type="button" class="btn btn-light" style='width:200px; height:150px; margin:10px'><i class="fas fa-truck-moving"></i><p></p>Proveedores</button>
-			<button type="button" class="btn btn-dark" style='width:200px; height:150px; margin:10px'><i class="fas fa-chart-line"></i><p></p>Finanzas</button>
+            <a href="../plantillas/ventas.php"><button type="button" class="btn btn-primary" style='width:280px; height:230px; margin:10px'><i class="fas fa-tag"></i><p></p>Ventas</button></a>
+            <a href="../plantillas/promociones.php"><button type="button" class="btn btn-secondary" style='width:200px; height:150px; margin:10px'><i class="fas fa-gift"></i><p></p>Promociones</button></a>
+			<a href="../plantillas/inventario.php"><button type="button" class="btn btn-success" style='width:200px; height:150px; margin:10px'><i class="fas fa-pallet"></i><p></p>Inventario</button></a>
+			<a href="../plantillas/reportes.php"><button type="button" class="btn btn-info" style='width:200px; height:150px; margin:10px'><i class="fas fa-clipboard-list"></i><p></p>Reportes</button></a>
+			<a href="../plantillas/clientes.php"><button type="button" class="btn btn-warning" style='width:200px; height:150px; margin:10px'><i class="fas fa-users"></i><p></p>Clientes</button></a>
+			<a href="../plantillas/compras.php"><button type="button" class="btn btn-danger" style='width:200px; height:150px; margin:10px'><i class="fas fa-donate"></i><p></p>Compras</button></a>
+			<a href="../plantillas/proveedores.php"><button type="button" class="btn btn-light" style='width:200px; height:150px; margin:10px'><i class="fas fa-truck-moving"></i><p></p>Proveedores</button></a>
+			<a href="../plantillas/finanzas.php"><button type="button" class="btn btn-dark" style='width:200px; height:150px; margin:10px'><i class="fas fa-chart-line"></i><p></p>Finanzas</button></a>
             </div>
         </div>
 
