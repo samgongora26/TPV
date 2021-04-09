463825c627ac83da96c27468808be604de97ce55
samgongora26 <saul.santiago2426@gmail.com>
2021-04-08 22:24:01 -0500

    correccion de tabla historial de compras

--- src/plantillas/compras/compras_historial.php
+++ src/plantillas/compras/compras_historial.php
@@ -51,16 +51,16 @@
                         <!-- Historial de compras-->
                             <div class="card">
                                 <div class="card-header bg-white">
-                                    <h3 class="card-tittle ">Historial de compras</h3>
+                                    <h3 class="card-tittle ">Historial de compras de hoy</h3>
                                 </div>
                                 <div class="card-body">
                                     <div class="table-responsive">
                                         <table id="zero_config" class="table table-striped table-bordered no-wrap">
                                             <thead>
+                                                <div id="mensaje2"></div>
                                                 <tr>
                                                     <th>ID</th>
-                                                    <th>Fecha y Hora</th>
-                                                    <th>Proveedor</th>
+                                                    <th>Fecha</th>
                                                     <th>Total</th>
                                                     <th>Pagado</th>
                                                     <th>Debido</th>
@@ -69,11 +69,10 @@
                                                     <th>Ver</th>
                                                 </tr>
                                             </thead>
-                                            <tbody>
+                                            <tbody id="contenido_tabla">
                                                 <tr>
                                                     <td>ddd31</td>
                                                     <td>29/01/2021 12:00 p.m</td>
-                                                    <td>luis alberto</td>
                                                     <td>1500</td>
                                                     <td>500</td>
                                                     <td>1000</td>
@@ -87,21 +86,6 @@
                                                 <tr>
                                                     <td>ddd31</td>
                                                     <td>29/01/2021 12:00 p.m</td>
-                                                    <td>luis alberto</td>
-                                                    <td>1500</td>
-                                                    <td>500</td>
-                                                    <td>1000</td>
-                                                    <td><div class="badge badge-danger text-wrap">Pago no completado</div></td>
-                                                    <td><button type="button" class="btn btn-success"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-plus-fill" viewBox="0 0 16 16">
-                                                        <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zM8.5 8a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V12a.5.5 0 0 0 1 0v-1.5H10a.5.5 0 0 0 0-1H8.5V8z"/></svg></button></td>
-                                                    <td><button type="button" class="btn btn-info"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-                                                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
-                                                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/></svg></button></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>ddd31</td>
-                                                    <td>29/01/2021 12:00 p.m</td>
-                                                    <td>luis alberto</td>
                                                     <td>1500</td>
                                                     <td>1500</td>
                                                     <td>0</td>
@@ -112,77 +96,73 @@
                                                         <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
                                                         <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/></svg></button></td>
                                                 </tr>
-                                                <tr>
-                                                    <td>ddd31</td>
-                                                    <td>29/01/2021 12:00 p.m</td>
-                                                    <td>luis alberto</td>
-                                                    <td>1500</td>
-                                                    <td>500</td>
-                                                    <td>1000</td>
-                                                    <td><div class="badge badge-danger text-wrap">Pago no completado</div></td>
-                                                    <td><button type="button" class="btn btn-success"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-plus-fill" viewBox="0 0 16 16">
-                                                        <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zM8.5 8a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V12a.5.5 0 0 0 1 0v-1.5H10a.5.5 0 0 0 0-1H8.5V8z"/></svg></button></td>
-                                                    <td><button type="button" class="btn btn-info"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-                                                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
-                                                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/></svg></button></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>ddd31</td>
-                                                    <td>29/01/2021 12:00 p.m</td>
-                                                    <td>luis alberto</td>
-                                                    <td>1500</td>
-                                                    <td>1500</td>
-                                                    <td>0</td>
-                                                    <td><div class="badge badge-success text-wrap">Pagado</div></td>
-                                                    <td><button type="button" class="btn btn-success"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-plus-fill" viewBox="0 0 16 16">
-                                                        <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zM8.5 8a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V12a.5.5 0 0 0 1 0v-1.5H10a.5.5 0 0 0 0-1H8.5V8z"/></svg></button></td>
-                                                    <td><button type="button" class="btn btn-info"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-                                                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
-                                                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/></svg></button></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>ddd31</td>
-                                                    <td>29/01/2021 12:00 p.m</td>
-                                                    <td>luis alberto</td>
-                                                    <td>1500</td>
-                                                    <td>1500</td>
-                                                    <td>0</td>
-                                                    <td><div class="badge badge-success text-wrap">Pagado</div></td>
-                                                    <td><button type="button" class="btn btn-success"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-plus-fill" viewBox="0 0 16 16">
-                                                        <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zM8.5 8a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V12a.5.5 0 0 0 1 0v-1.5H10a.5.5 0 0 0 0-1H8.5V8z"/></svg></button></td>
-                                                    <td><button type="button" class="btn btn-info"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-                                                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
-                                                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/></svg></button></td>
-                                                </tr>
-                                                <tr>
-                                                    <td>ddd31</td>
-                                                    <td>29/01/2021 12:00 p.m</td>
-                                                    <td>luis alberto</td>
-                                                    <td>1500</td>
-                                                    <td>500</td>
-                                                    <td>1000</td>
-                                                    <td><div class="badge badge-danger text-wrap">Pago no completado</div></td>
-                                                    <td><button type="button" class="btn btn-success"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag-plus-fill" viewBox="0 0 16 16">
-                                                        <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zM8.5 8a.5.5 0 0 0-1 0v1.5H6a.5.5 0 0 0 0 1h1.5V12a.5.5 0 0 0 1 0v-1.5H10a.5.5 0 0 0 0-1H8.5V8z"/></svg></button></td>
-                                                    <td><button type="button" class="btn btn-info"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-                                                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
-                                                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/></svg></button></td>
-                                                </tr>
+                                                
                                             </tbody>
                                         </table>
-                                        <ul class="pagination float-right">
-                                            <li class="page-item disabled">
-                                                <a class="page-link" href="#" tabindex="-1">Previous</a>
-                                            </li>
-                                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                                            <li class="page-item">
-                                                <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-                                            </li>
-                                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                            <li class="page-item">
-                                                <a class="page-link" href="#">Next</a>
-                                            </li>
-                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+
+                        <!-- Historial de compras-->
+                            <div class="card">
+                                <div class="card-header bg-white">
+                                    <h3 class="card-tittle ">Compras de ayer</h3>
+                                </div>
+                                <div class="card-body">
+                                    <div class="table-responsive">
+                                        <table id="zero_config" class="table table-striped table-bordered no-wrap">
+                                            <thead>
+                                                <div id="mensaje2"></div>
+                                                <tr>
+                                                    <th>ID</th>
+                                                    <th>Fecha y Hora</th>
+                                                    <th>Total</th>
+                                                    <th>Pagado</th>
+                                                    <th>Debido</th>
+                                                    <th>Estado</th>
+                                                    <th>Pago</th>
+                                                    <th>Ver</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody id="contenido_tabla">
+
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+
+                        <!-- Historial de compras-->
+                            <div class="card">
+                                <div class="card-header bg-white">
+                                    <h3 class="card-tittle ">Buscar una fecha en especifico</h3>
+                                    <div class="form-input">
+                                        <label for="">Elige una fecha</label>
+                                        <input type="date">
+                                        <button class="btn btn-sm btn-success">Buscar</button>
+                                    </div>
+                                    
+                                </div>
+                                <div class="card-body">
+                                    <div class="table-responsive">
+                                        <table id="zero_config" class="table table-striped table-bordered no-wrap">
+                                            <thead>
+                                                <div id="mensaje2"></div>
+                                                <tr>
+                                                    <th>ID</th>
+                                                    <th>Fecha</th>
+                                                    <th>Total</th>
+                                                    <th>Pagado</th>
+                                                    <th>Debido</th>
+                                                    <th>Estado</th>
+                                                    <th>Pago</th>
+                                                    <th>Ver</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody id="contenido_tabla">
+
+                                            </tbody>
+                                        </table>
                                     </div>
                                 </div>
                             </div>
