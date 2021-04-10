79827d6d033a5fe37725c13e469b191a6f18707b
samgongora26 <saul.santiago2426@gmail.com>
2021-04-10 14:45:09 -0500

    modal en promociones

--- src/plantillas/promociones/promociones.php
+++ src/plantillas/promociones/promociones.php
@@ -45,119 +45,115 @@
         <!-- CONTENEDOR -->
         <div class="page-wrapper">
             <div class="container-fluid">
-                <!-- AQUI EMPEZAMOS A AGREGAR DISEÑO DEL CENTRO -->
-                <div class="col-sm-12 col-md-12 col-lg-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <h4 class="card-title">Promociones</h4><p></p>
-                            <button type="submit" class="btn btn-info">Agregar Promocion</button>
-                        </div>
-                        <p></p>
-                        <div class="table-responsive">
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th>Promocion</th>
-                                        <th>Codigo</th>
-                                        <th>Nombre</th>
-                                        <th scope="col">Editar</th>
-                                        <th scope="col">Eliminar</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Cell</td>
-                                        <td>Cell</td>
-                                        <td>Cell</td>
-                                        <td>
-                                            <button type="button" class="btn btn-primary" data-toggle="modal"
-                                            data-target="#edit-modal"><i class="icon-pencil"></i></button>
-                                        </td>
-                                        <td>
-                                            <button type="button" class="btn btn-primary"><i class="icon-trash"></i></button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Cell</td>
-                                        <td>Cell</td>
-                                        <td>Cell</td>
-                                        <td>
-                                            <button type="button" class="btn btn-primary" data-toggle="modal"
-                                            data-target="#edit-modal"><i class="icon-pencil"></i></button>
-                                        </td>
-                                        <td>
-                                            <button type="button" class="btn btn-primary"><i class="icon-trash"></i></button>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+                <div class="row">
+                    <div class="col-sm-4 col-md-4 col-lg-4">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Agregar promoción</h4>
+
+                                    <div class="form-group mb-4">
+                                        <label class="mr-sm-2" for="inlineFormCustomSelect">Tipo de Promocion</label>
+                                        <select class="custom-select mr-sm-2" id="promociones">
+                                            <option selected value="5">5%</option>
+                                            <option value="2">10%</option>
+                                            <option value="2">15%</option>
+                                            <option value="1">20%</option>
+                                            <option value="2">30%</option>
+                                            <option value="2">40%</option>
+                                            <option value="2">50%</option>   
+                                        </select><p></p>
+                                        <label>Producto en Promocion</label>
+                                        <input placeholder="codigo" type="text" class="form-control"><p></p>
+                                        <!--label>Nombre de la Promocion</label>
+                                        <input placeholder="Nombre" type="text" class="form-control"><p></p-->
+                                    </div>
+                                    <button type="submit" class="btn btn-block btn-success">Agregar Promocion</button>
+
+                                </div>
+                            </div>
+                    </div>
+                    <!-- AQUI EMPEZAMOS A AGREGAR DISEÑO DEL CENTRO -->
+                    <div class="col-sm-12 col-md-8 col-lg-8">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4 class="card-title">Promociones</h4>
+                            </div>
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Producto</th>
+                                            <th>Codigo</th>
+                                            <th>descuento</th>
+                                            <th scope="col">Acciones</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Cell</td>
+                                            <td>Cell</td>
+                                            <td>Cell</td>
+                                            <td>
+                                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal_editar"><i class="icon-pencil"></i></button>
+                                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal_eliminar"><i class="icon-trash"></i></button>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
+                </div>
+
+                <!--MODALS-->
+                <!--MODAL EDITAR-->
+                <div id="modal_editar" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" style="display: none;" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h4 class="modal-title" id="myModalLabel">Modal Heading</h4>
+                                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="form-group mb-4">
+                                                    <label class="mr-sm-2" for="inlineFormCustomSelect">Tipo de Promocion</label>
+                                                    <select class="custom-select mr-sm-2" id="promociones_editar">
+                                                            <option selected value="5">5%</option>
+                                                            <option value="2">10%</option>
+                                                            <option value="2">15%</option>
+                                                            <option value="1">20%</option>
+                                                            <option value="2">30%</option>
+                                                            <option value="2">40%</option>
+                                                            <option value="2">50%</option>   
+                                                        </select><p></p>
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-warning" data-dismiss="modal">Cancelar</button>
+                                                <button type="button" class="btn btn-success">Guardar cambios</button>
+                                            </div>
+                                        </div><!-- /.modal-content -->
+                                    </div><!-- /.modal-dialog -->
+                </div>
+                <!--MODAL ELIMINAR-->
+                <div id="modal_eliminar" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" style="display: none;" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h4 class="modal-title" id="myModalLabel">Eliminar la promocion</h4>
+                                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <h4>¿Está seguro de eliminar la promocion de este producto?</h4>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-warning" data-dismiss="modal">Cancelar</button>
+                                                <button type="button" class="btn btn-success">Guardar cambios</button>
+                                            </div>
+                                        </div><!-- /.modal-content -->
+                                    </div><!-- /.modal-dialog -->
                 </div>
                 
-                <div class="row">
-                    <!-- EMPIEZA -->
-                    <div class="col-sm-4 col-md-4 col-lg-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <h4 class="card-title">Boton de Agregar</h4>
-
-                                <div class="form-group mb-4">
-                                    <label class="mr-sm-2" for="inlineFormCustomSelect">Tipo de Promocion</label>
-                                    <select class="custom-select mr-sm-2">
-                                        <option selected value="1">2x1</option>
-                                        <option value="2">3x2</option>
-                                        <option value="2">5%</option>
-                                        <option value="2">10%</option>
-                                        <option value="2">15%</option>
-                                    </select><p></p>
-                                    <label>Producto en Promocion</label>
-                                    <input placeholder="Producto" type="text" class="form-control"><p></p>
-                                    <label>Nombre de la Promocion</label>
-                                    <input placeholder="Nombre" type="text" class="form-control"><p></p>
-                                </div>
-                                <button type="submit" class="btn btn-info">Agregar Promocion</button>
-
-                            </div>
-                        </div>
-                    </div>
-                    <!-- TERMINA -->
-                    <!-- EMPIEZA -->
-                    <div class="col-sm-4 col-md-4 col-lg-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <h4 class="card-title">Boton de editar</h4>
-                                <div class="form-group mb-4">
-                                    <label class="mr-sm-2" for="inlineFormCustomSelect">Tipo de Promocion</label>
-                                    <select class="custom-select mr-sm-2">
-                                        <option selected value="1">2x1</option>
-                                        <option value="2">3x2</option>
-                                        <option value="2">5%</option>
-                                        <option value="2">10%</option>
-                                        <option value="2">15%</option>
-                                    </select><p></p>
-                                    <label>Nombre de la Promocion</label>
-                                    <input placeholder="Nombre" type="text" class="form-control"><p></p>
-                                </div>
-                                <button type="submit" class="btn btn-info">Editar Promocion</button>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- TERMINA -->
-                    <!-- EMPIEZA -->
-                    <div class="col-sm-4 col-md-4 col-lg-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <div>
-                                    <h4 class="card-title">Boton de Eliminar</h4>
-                                    <label>Nombre de la Promocion</label>
-                                </div>
-                                <button type="submit" class="btn btn-info">Eliminar Promocion</button>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- TERMINA -->
-                </div>
                 
             </div>
         </div>
