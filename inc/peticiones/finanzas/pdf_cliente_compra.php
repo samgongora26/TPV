d7fa0c5f903348d30347c0e98f8795d87e99dce8
silvercrow185 <oossllyy14@gmail.com>
2021-04-22 18:25:45 -0500

    pdfs de finanzas

--- inc/peticiones/finanzas/pdf_cliente_compra.php
+++ inc/peticiones/finanzas/pdf_cliente_compra.php
@@ -2,7 +2,7 @@
 
     require('../../../src/assets/extra-libs/pdf/fpdf.php');
 
-    class PDF1 extends FPDF
+    class PDF extends FPDF
     {
     // Cabecera de página
     function Header()
@@ -12,14 +12,14 @@
         // Movernos a la derecha
         $this->Cell(80);
         // Título
-        $this->Cell(30,10,utf8_decode('Empleados Con Más Ventas'),0,0,'C');
+        $this->Cell(30,10,utf8_decode('Clientes Con Más Compras'),0,0,'C');
         // Salto de línea
         $this->Ln(20);
         //titulos de la tabla
-        $this->Cell(20, 10, '', 0, 0, 'C', 0);
-        $this->Cell(30, 10, '*', 1, 0, 'C', 0);
-        $this->Cell(60, 10, 'Nombres', 1, 0, 'C', 0);
-        $this->Cell(60, 10, utf8_decode('N° de Compras'), 1, 1, 'C', 0);
+        $this->Cell(30, 10, '', 0, 0, 'C', 0);
+        $this->Cell(40, 10, 'count', 1, 0, 'C', 0);
+        $this->Cell(30, 10, 'id', 1, 0, 'C', 0);
+        $this->Cell(60, 10, 'nombres', 1, 1, 'C', 0);
     }
 
     // Pie de página
@@ -36,24 +36,23 @@
 
     //consulta
     require '../../../conexion.php'; //archivo que hace la conexion a la bd
-    $consulta = "SELECT COUNT(usuarios.nombres) AS contador, usuarios.id_usuario, 
-    usuarios.nombres, usuarios.apellidos FROM `usuarios`,`ventas`,`detalle_venta` 
-    WHERE usuarios.id_usuario = ventas.id_empleado AND detalle_venta.id_venta = ventas.id_venta 
-    GROUP BY usuarios.nombres ORDER BY contador DESC;";
+    $consulta = "SELECT COUNT(clientes.nombres) AS contador,clientes.id_cliente, clientes.nombres 
+    FROM clientes, ventas WHERE clientes.id_cliente = ventas.id_cliente GROUP BY ventas.id_cliente 
+    ORDER BY contador DESC;";
     $resultado = mysqli_query($conexion, $consulta);
 
     //generador del pdf
-    $pdf = new PDF1();
+    $pdf = new PDF();
     $pdf->AliasNbPages();
     $pdf->AddPage();
     $pdf->SetFont('Arial','',16);
 
     //tabla de los datos
     while ($row = $resultado->fetch_assoc()) {
-        $pdf->Cell(20, 10, '', 0, 0, 'C', 0);
-        $pdf->cell(30, 10, utf8_decode ($row['id_usuario']), 1, 0, 'C', 0);
-        $pdf->cell(60, 10, utf8_decode ($row['nombres']), 1, 0, 'C', 0);
-        $pdf->cell(60, 10, utf8_decode ($row['contador']), 1, 1, 'C', 0);
+        $pdf->Cell(30, 10, '', 0, 0, 'C', 0);
+        $pdf->cell(40, 10, utf8_decode ($row['contador']), 1, 0, 'C', 0);
+        $pdf->cell(30, 10, utf8_decode ($row['id_cliente']), 1, 0, 'C', 0);
+        $pdf->cell(60, 10, utf8_decode ($row['nombres']), 1, 1, 'C', 0);
     }
 
     $pdf->Output();
---
+++ inc/peticiones/finanzas/pdf_producto_vendido.php
@@ -0,0 +1,62 @@
+<?php
+
+    require('../../../src/assets/extra-libs/pdf/fpdf.php');
+
+    class PDF extends FPDF
+    {
+    // Cabecera de página
+    function Header()
+    {
+        // Arial bold 15
+        $this->SetFont('Arial','B',18);
+        // Movernos a la derecha
+        $this->Cell(80);
+        // Título
+        $this->Cell(30,10,utf8_decode('Productos Con Más Ventas'),0,0,'C');
+        // Salto de línea
+        $this->Ln(20);
+        //titulos de la tabla
+        $this->Cell(20, 10, '#', 1, 0, 'C', 0);
+        $this->Cell(40, 10, 'Codigo', 1, 0, 'C', 0);
+        $this->Cell(50, 10, 'Producto', 1, 0, 'C', 0);
+        $this->Cell(80, 10, 'Precio venta', 1, 1, 'C', 0);
+    }
+
+    // Pie de página
+    function Footer()
+    {
+        // Posición: a 1,5 cm del final
+        $this->SetY(-15);
+        // Arial italic 8
+        $this->SetFont('Arial','I',8);
+        // Número de página
+        $this->Cell(0,10,utf8_decode('Pagina ').$this->PageNo().'/{nb}',0,0,'C');
+    }
+    }
+
+    //consulta
+    require '../../../conexion.php'; //archivo que hace la conexion a la bd
+    $consulta = "SELECT COUNT(productos_inventario.nombre_producto) 
+    AS contador, productos_inventario.id_producto, productos_inventario.codigo, 
+    productos_inventario.precio_venta, productos_inventario.nombre_producto 
+    FROM productos_inventario, detalle_venta WHERE productos_inventario.id_producto = detalle_venta.id_producto 
+    GROUP BY detalle_venta.id_producto ORDER BY contador DESC;";
+    $resultado = mysqli_query($conexion, $consulta);
+
+    //generador del pdf
+    $pdf = new PDF();
+    $pdf->AliasNbPages();
+    $pdf->AddPage();
+    $pdf->SetFont('Arial','',16);
+
+    //tabla de los datos
+    while ($row = $resultado->fetch_assoc()) {
+        $pdf->cell(20, 10, utf8_decode ($row['contador']), 1, 0, 'C', 0);
+        $pdf->cell(40, 10, utf8_decode ($row['codigo']), 1, 0, 'C', 0);
+        $pdf->cell(50, 10, utf8_decode ($row['precio_venta']), 1, 0, 'C', 0);
+        $pdf->cell(80, 10, utf8_decode ($row['nombre_producto']), 1, 1, 'C', 0);
+    }
+
+    $pdf->Output();
+
+?>
